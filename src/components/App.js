4b36b5bdf8a0a9e82a319d535b7c866e5a504340
Calvin Cheung <calvchum@gmail.com>
2019-05-20 16:55:27 +1000

    changed name of Contact.js to Contacts.js, created Projects.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Header from './Header';
 import Landing from './Landing';
 import Footer from './Footer';
 import About from './About';
+import Contacts from './Contacts';
+import Projects from './Projects';
 
 class App extends Component {
   render() {  
@@ -13,6 +15,8 @@ class App extends Component {
           <Route path="/" component={Header}/>
           <Route exact path="/" component={Landing}/>
           <Route exact path="/about/" component={About}/>
+          <Route exact path="/contacts/" component={Contacts}/>
+          <Route exact path="/projects/" component={Projects}/>
           <Footer/>
         </div>
       </BrowserRouter>
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contacts extends Component {
+  render() {  
+    return (
+      <h1>
+        Contact us on 13 13 32 and we'll come to you
+      </h1>
+    );
+  }
+}
+
+export default Contacts;
