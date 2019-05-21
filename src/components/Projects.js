34ff4c7eb2c923480f91e083a02c397090e40edd
Calvin Cheung <calvchum@gmail.com>
2019-05-21 16:45:27 +1000

    Projects now maps through projectData and creates a Project.js for every object in that array

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Project from './Project'
+import * as data from '../constants/projectInfo';
 
 class Projects extends Component {
   render() {  
+  let projects = data.projectData.map((project, i) => {
+    return(
+      <Project
+        key={i}
+        project={project}
+      />
+    )
+  }) 
+
     return (
-      <Project/>
-    );
+      <div>
+        <h1>Projects</h1>
+        <ul>{projects}</ul>
+      </div>
+    )
   }
 }
 
