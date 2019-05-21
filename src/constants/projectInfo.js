9295a8c391b0c842be7841b6188de724990e458c
Calvin Cheung <calvchum@gmail.com>
2019-05-21 16:10:23 +1000

    created projectInfo.js

--- src/constants/projectInfo.js
+++ src/constants/projectInfo.js
@@ -2,6 +2,12 @@ export const projectData = [
 	{
     name: 'Wholefoods Shopify React',
     img: 'https://cdn-images-1.medium.com/max/2600/1*7Io_m8RdW-bAyldx4ixF7A.png',
+    desc: 'we built this from scratch'
 	},
+  {
+    name: 'example project 2',
+    img: 'https://cdn-images-1.medium.com/max/2600/1*7Io_m8RdW-bAyldx4ixF7A.png',
+    desc: 'we built this from scratch'
+  }
 
 ]
