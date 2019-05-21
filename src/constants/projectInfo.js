783d79f45db5e68323345d717f93d25f9797dca1
Calvin Cheung <calvchum@gmail.com>
2019-05-21 16:14:20 +1000

    changed img for exampel project 2

--- src/constants/projectInfo.js
+++ src/constants/projectInfo.js
@@ -6,7 +6,7 @@ export const projectData = [
 	},
   {
     name: 'example project 2',
-    img: 'https://cdn-images-1.medium.com/max/2600/1*7Io_m8RdW-bAyldx4ixF7A.png',
+    img: 'https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     desc: 'we built this from scratch'
   }
 
