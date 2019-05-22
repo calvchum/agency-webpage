a60c3dd4752f6310f6b0dfbbaaad6caad3d899e9
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-05-22 17:24:01 +1000

    added font, color and text variables

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as data from '../constants/menuItems.js';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class Footer extends Component {
   render() {  
     let menuLinks = data.menuItems.map((item, i) => {
       return (
-        <Link to={item.link} key={i} style={{textDecoration: 'none'}}>
-          <li style={{display: 'inline'}} key={i}>{item.title} / </li>
+        <Link to={item.link} key={i}>
+          <li className="footer-link-item" key={i}>{item.title} / </li>
         </Link>
       )
     })
 
     return (
-      <ul>
-        {menuLinks}
-      </ul>
+      <Container>
+        <Row>
+          <Col>
+            <ul className="footer-navigation">
+              {menuLinks}
+            </ul>
+          </Col>
+
+          <Col>
+            
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
