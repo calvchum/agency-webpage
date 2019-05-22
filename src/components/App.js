4b5d4f432ff0abae68d06469ed50043ba69d2825
Calvin Cheung <calvchum@gmail.com>
2019-05-22 14:22:57 +1000

    created burgerJs but need scss to continue

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,52 @@ import Footer from './Footer';
 import About from './About';
 import Contacts from './Contacts';
 import Projects from './Projects';
+import Burger from './Burger';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isBurgerOpen: false,
+    }
+    this.handleBurgerOpen = this.handleBurgerOpen.bind(this)
+    this.handleBurgerClose = this.handleBurgerClose.bind(this)
+  }
+
+  handleBurgerOpen() {
+    this.setState({
+      isBurgerOpen: true
+    })
+  }
+
+  handleBurgerClose() {
+    this.setState({
+      isBurgerOpen: false
+    })
+  }
+
+
   render() {  
+
+    const renderHeader = (props) => {
+      return (
+        <Header
+          handleBurgerOpen={this.handleBurgerOpen}
+          handleBurgerClose={this.handleBurgerClose}
+          {...props}
+        />
+      )    
+    }
+
     return (
       <BrowserRouter>
         <div>
-          <Route path="/" component={Header}/>
+          <Route path="/" render={renderHeader}/>
           <Route exact path="/" component={Landing}/>
           <Route exact path="/about/" component={About}/>
           <Route exact path="/contacts/" component={Contacts}/>
           <Route exact path="/projects/" component={Projects}/>
+          <Burger/>
           <Footer/>
         </div>
       </BrowserRouter>
