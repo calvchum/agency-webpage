63f0d2339300f0ea9c4722755bf2c4ae92fe230e
Calvin Cheung <calvchum@gmail.com>
2019-05-22 17:47:22 +1000

    added handleClick function that changes the displayInfo state

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react'
+import * as data from '../constants/servicesInfo'
 
 class Services extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayInfo: []
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }  
+
+  handleClick(category) {
+    this.setState({
+      displayInfo: category
+    })
+  }
 	render() {
+    let services = data.servicesData.map((service, i)=> {
+      return(
+        <button key={i} onClick={() => this.handleClick(service.category)}>
+          <li style={{display: 'inline', margin: '5px'}} key={i}>{service.header}</li>
+        </button>
+      )
+    })
+
+    let displayInfo = this.state.displayInfo.map((category, i) => {
+      return (
+        <div key={i}>
+          <p>{category.subHeader}</p>
+          <ul>
+            {category.listItems.map((listItem, i) => {
+              return (
+                <li key={i}>{listItem}</li>
+              )
+            })}          
+          </ul>
+        </div>
+      )
+    })
+
 		return (
-      <p>hello</p>
+      <div>
+        <ul>
+          {services}
+        </ul>
+        <div>
+          {displayInfo}
+        </div>
+      </div>
 		)
 	}
 }
---
+++ src/constants/servicesInfo.js
@@ -0,0 +1,86 @@
+export const servicesData = [
+	{
+    header: 'UX',
+    category: [
+      {
+      subHeader: 'Briefing',
+      listItems: ['User Interviews', 'Group classes', 'Brain storming']
+      },
+      {
+        subHeader: 'Research',
+        listItems: ['Asking a mate', 'Google']
+      },
+      {
+        subHeader: 'Synethesize',
+        listItems: ['Chemistry', 'Bunsen burner']
+      },
+      {
+        subHeader: 'Deliverables',
+        listItems: ['Deadlines', 'KPI']
+      }
+    ]
+	},
+  {
+    header: 'UI',
+    category: [
+      {
+      subHeader: 'Dribble',
+      listItems: ['Google images', 'Stealing', 'Plagarism']
+      },
+      {
+        subHeader: 'Codepen',
+        listItems: ['nice style', 'Copy paste']
+      },
+      {
+        subHeader: 'Coolors',
+        listItems: ['Blue', 'Red']
+      },
+      {
+        subHeader: 'Colormind',
+        listItems: ['Orange', 'Purple']
+      }
+    ]
+  },
+  {
+    header: 'Development',
+    category: [
+      {
+      subHeader: 'Coding',
+      listItems: ['error', 'debugging', 'frustration']
+      },
+      {
+        subHeader: 'React',
+        listItems: ['react-router-dom', 'redux']
+      },
+      {
+        subHeader: 'Node',
+        listItems: ['Express.js', 'Passport.js']
+      },
+      {
+        subHeader: 'Maintenance',
+        listItems: ['$40/month', '24/7 hotline']
+      }
+    ]    
+  },
+  {
+    header: 'Tools',
+    category: [
+      {
+      subHeader: 'Slack',
+      listItems: ['#australia', '#lewagon', '#harrisonKim']
+      },
+      {
+        subHeader: 'Whatsapp',
+        listItems: ['call me maybe', 'hi baby']
+      },
+      {
+        subHeader: 'Macbook',
+        listItems: ['2013 retina', '15 inches ;) ;) ;) ;)']
+      },
+      {
+        subHeader: 'Internet',
+        listItems: ['56k', 'dialup']
+      }
+    ]
+  },
+]
