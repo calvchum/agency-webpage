ca07446fad57cb5a7b76bfff4025124506a71624
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-05-24 15:52:35 +1000

    position social media icons and imported them in th header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import MediaIcons from './MediaIcons';
 
 class Header extends Component {
   render() {  
     return (
-
       <Row>
+        <MediaIcons/>
         <Col className='header-container'>
           <button id="hamburger-menu-icon" className={this.props.isBurgerOpen ? 'active' : ''} onClick={this.props.handleBurgerOpen}><span></span><span></span></button>
           <div className="header-positioning">
-            <p className="header-text">agency name.</p>
+            <Link style={{textDecoration: 'none', color: 'white'}} to='/'> 
+              <p className="header-text">agency name.</p>
+            </Link>
           </div>
         </Col>
       </Row>
