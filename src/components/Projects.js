1814c28bdab9e38b1d02e611bb6e446bd2e027cb
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-05-24 18:25:34 +1000

    projects page styled: g

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Project from './Project'
 import * as data from '../constants/projectInfo';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 class Projects extends Component {
   render() {  
@@ -14,10 +16,19 @@ class Projects extends Component {
   }) 
 
     return (
-      <div>
-        <h1>Projects</h1>
-        <ul>{projects}</ul>
-      </div>
+      <Container>
+        <Row>
+          <Col xs='12' md={{ offset: '1', span: 10 }} className="page-header">
+          <h1 className="subheader-text">Projects</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs='12' md={{ offset: '1', span: 10 }}>
+            {projects}
+            
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
