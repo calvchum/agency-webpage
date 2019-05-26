cae362c25c6f5edbe9cc4f48159afdf74998910b
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-05-27 08:24:42 +1000

    added a page loader transition and strcutured the landing page to reflect figma changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
+import Layover from './Layover';
+
 
 class Landing extends Component {
+  // Function hides the layover animation so the links on the landing page can be clicked.
+  componentDidMount() {
+    const layover = document.querySelector('.layover-wrapper');
+    setTimeout(() => layover.classList.add('hidden'), 3000);
+  }
+
   render() {  
     return (
       <div>
+        <Layover/>
         <Row className="hero-wrapper">
           <Col xs="12" md={{ offset: 2, span: 8 }} className="secondary-content">
-            <h1 className="header-text">UI / UX / Development</h1>
+          <Row className="landing-term-definition">
+            <Col>
+              <h1 className="header-text">UX</h1>
+            </Col>
+            <Col>
+              <p className="body-text">UX is the flow through your website</p>
+            </Col>
+          </Row>
+          <Row className="landing-term-definition">
+            <Col>
+              <h1 className="header-text">UI</h1>
+            </Col>
+            <Col>
+              <p className="body-text">UX is the flow through your website</p>
+            </Col>
+          </Row>
+          <Row className="landing-term-definition">
+            <Col>
+              <h1 className="header-text">Dev</h1>
+            </Col>
+            <Col>
+              <p className="body-text">UX is the flow through your website</p>
+            </Col>
+          </Row>
             <a href="#secondary-wrapper" className="next-page bounce"><img src={ require('../assets/down-arrow.svg') } alt="downward point arrow"/></a>
           </Col>
         </Row>        
