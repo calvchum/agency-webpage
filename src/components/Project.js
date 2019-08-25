98cadbc3867d0080b01d714a034d664ab8c8b1f9
Calvin Cheung <calvchum@gmail.com>
2019-08-25 23:53:57 +0800

    changed the ecze photo

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 class Project extends Component {
-  render() {  
-    const { name, img, desc, tag, medium } = this.props.project
+  render() {
+    const { name, img, desc, tag, medium } = this.props.project;
 
-    let tags = []
+    let tags = [];
 
-    tags.push(tag.slice(0, -1).map((e, i)=>{
-          return (
-            <span  key={i}>
-              {e} /<span> </span> 
-            </span>
-          )
-        }))
+    tags.push(
+      tag.slice(0, -1).map((e, i) => {
+        return (
+          <span key={i}>
+            {e} /<span> </span>
+          </span>
+        );
+      })
+    );
 
-    tags.push(tag.slice(-1))
+    tags.push(tag.slice(-1));
 
     return (
       <div className="link-wrapper">
-        <a href={medium.frontend} target='_blank'>
-          <Row className="project-wrapper" style={{backgroundImage: `url(${img})`}}>
+        <a href={medium.frontend} target="_blank">
+          <Row
+            className="project-wrapper"
+            style={{ backgroundImage: `url(${img})`, backgroundSize: "cover" }}
+          >
             <Col className="display-none project-content-alignment">
               <div className="project-header">
                 <h1 className="header-text">{name}</h1>
--- src/constants/projectInfo.js
+++ src/constants/projectInfo.js
@@ -1,23 +1,26 @@
 export const projectData = [
-	{
-    name: 'Wholefoods Shopify React',
-    img: 'https://cdn-images-1.medium.com/max/2600/1*7Io_m8RdW-bAyldx4ixF7A.png',
-    desc: 'Designed and created an e-commerce website from scratch using React and Shopify API',
-    tag: ['UI', 'React', 'E-commerce'],
-    medium: {
-      backend: 'https://medium.com/@calvchum/building-a-shopify-e-commerce-store-using-react-719000bf7440',
-      frontend: 'https://medium.com/@BQWA/organic-wholefoods-ux-case-study-f41e42d38ea1'
-    }
-	},
   {
-    name: 'example project 2',
-    img: 'https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    desc: 'This project taught us the power of love',
-    tag: ['UI', 'React', 'Node', 'UX'],
+    name: "Wholefoods Shopify React",
+    img:
+      "https://cdn-images-1.medium.com/max/2600/1*7Io_m8RdW-bAyldx4ixF7A.png",
+    desc:
+      "Designed and created an e-commerce website from scratch using React and Shopify API",
+    tag: ["UI", "React", "E-commerce"],
     medium: {
-      backend: '',
-      frontend: ''
+      backend:
+        "https://medium.com/@calvchum/building-a-shopify-e-commerce-store-using-react-719000bf7440",
+      frontend:
+        "https://medium.com/@BQWA/organic-wholefoods-ux-case-study-f41e42d38ea1"
+    }
+  },
+  {
+    name: "Ecze",
+    img: "https://i.ibb.co/0KZ4pgc/ecze-screenshot-1.png",
+    desc: "This project taught us the power of love",
+    tag: ["UI", "React", "Node", "UX"],
+    medium: {
+      backend: "",
+      frontend: ""
     }
   }
-
-]
+];
